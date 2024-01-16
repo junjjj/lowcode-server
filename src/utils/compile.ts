5d85ad506e873619734559fb63b2ac83e7b02bd9
duyacode <18795905365@163.com>
2024-01-16 17:44:44 +0800

    feat: 1. 代码提交

--- src/utils/compile.ts
+++ src/utils/compile.ts
@@ -1,4 +1,6 @@
-export const compile = function (json) {
+import { existsSync, writeFileSync } from 'fs';
+import { exec, execSync } from 'child_process';
+export const compile = async function (json) {
   console.log('传递过来的 JSON 文件', json);
 
   const data = {
@@ -12,6 +14,59 @@ export const compile = function (json) {
   const template = compileTemplate(json, data);
 
   console.log(template, '\n', data);
+  // 输出目录
+  const output = './output';
+
+  const isExist = await existsSync(output);
+
+  console.log(isExist);
+  if (isExist) {
+    // 创建
+    await execSync(`rm -rf ${output}`);
+  }
+
+  await execSync(`mkdir ${output}`);
+
+  let script = `<script setup>\n`;
+  if (Object.keys(data.handlers).length) {
+    script += `import { ${Object.keys(data.handlers).join(
+      ', ',
+    )} } from './handlers'\n`;
+  }
+
+  if (Object.keys(data.props).length) {
+    let keys = '{';
+
+    for (const key in data.props) {
+      keys += `${key}: ${data.props[key]}`;
+    }
+
+    keys += '}';
+    script += `
+      const { ${Object.keys(data.props).join(', ')} } = defineProps(${keys})
+    `;
+  }
+
+  script += `</script>`;
+
+  await writeFileSync(
+    `${output}/index.vue`,
+    `<template>
+      ${template}
+    </template>
+    ${script}
+    `,
+    'utf-8',
+  );
+
+  let handler = '';
+  for (const key in data.handlers) {
+    handler += `export const ${key} = ${data.handlers[key]}\n`;
+  }
+
+  // 生成 handler.ts
+  await writeFileSync(`${output}/handlers.ts`, `${handler}`, 'utf-8');
+  console.log('生成完成✅');
 };
 
 const compileTemplate = (json, data) => {
